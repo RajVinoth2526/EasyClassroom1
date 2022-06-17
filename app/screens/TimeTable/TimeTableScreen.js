1fa75292be4bbf44a794cdffb60ef91da6837dea
RajVinoth2526 <vinothraj2526@gmail.com>
2022-06-17 21:24:12 +0530

    timetable

--- app/screens/TimeTable/TimeTableScreen.js
+++ app/screens/TimeTable/TimeTableScreen.js
@@ -162,6 +162,11 @@ export default function TimeTableScreen({ navigation }) {
     wait(2000).then(() => setRefreshing(false));
   }, []);
 
+
+  const generateRandomBrightestHSLColor = () => {
+    return "hsla(" + ~~(360 * Math.random()) + "," + "80%," + "90%,2)";
+  };
+
   if (role == "Lecturer") {
     return (
       <View style={styles.container}>
@@ -169,10 +174,15 @@ export default function TimeTableScreen({ navigation }) {
           <View style={{ marginTop: 20, marginBottom: 20 }}>
             <Text style={{ fontSize: 20, alignSelf: "center" }}>
               {" "}
-              <Octicons name="note" size={25} color="#34dbeb" /> Time Table
+              <MaterialCommunityIcons
+              name="table-clock"
+              size={25}
+              color="black"
+            />{" "}Time Table
             </Text>
           </View>
-          <View style={{ marginTop: 30 }}>
+         <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
+          <View style={{  marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
             >
@@ -185,13 +195,15 @@ export default function TimeTableScreen({ navigation }) {
               <Image
                 source={{ uri: image1 }}
                 style={{
+                  borderRadius :3,
                   height: 200,
-                  width: 350,
+                  width: 300,
                 }}
               />
             </View>
           </TouchableOpacity>
-
+         </View>
+         <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -205,12 +217,15 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image2 }}
               style={{
+                borderRadius :3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
 
+          </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -224,11 +239,13 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image3 }}
               style={{
+                borderRadius :3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
+        </View>
         </ScrollView>
       </View>
     );
@@ -242,6 +259,7 @@ export default function TimeTableScreen({ navigation }) {
               <Octicons name="note" size={25} color="#34dbeb" /> Time Table
             </Text>
           </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -255,12 +273,14 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image1 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
-
+        </View>
+        <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -274,12 +294,14 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image2 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
-
+        </View>
+        <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -293,11 +315,13 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image3 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
+        </View>
         </ScrollView>
       </View>
     );
@@ -311,6 +335,7 @@ export default function TimeTableScreen({ navigation }) {
               <Octicons name="note" size={25} color="#34dbeb" /> Time Table
             </Text>
           </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -324,12 +349,14 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image1 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
-
+        </View>
+        <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -343,12 +370,14 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image2 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
-
+        </View>
+        <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -362,11 +391,13 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image3 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
           </View>
+        </View>
         </ScrollView>
       </View>
     );
@@ -380,6 +411,7 @@ export default function TimeTableScreen({ navigation }) {
               <Octicons name="note" size={25} color="#34dbeb" /> Time Table
             </Text>
           </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -393,10 +425,12 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image1 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
+          
             <View style={styles.uploadButton}>
               <MaterialCommunityIcons
                 onPress={pickImage1}
@@ -406,7 +440,8 @@ export default function TimeTableScreen({ navigation }) {
               />
             </View>
           </View>
-
+          </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -420,8 +455,9 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image2 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
 
@@ -434,7 +470,8 @@ export default function TimeTableScreen({ navigation }) {
               />
             </View>
           </View>
-
+          </View>
+          <View style = {[styles.Box, {backgroundColor : generateRandomBrightestHSLColor()}]}>
           <View style={{ marginTop: 30 }}>
             <Text
               style={{ fontSize: 20, alignItems: "flex-start", marginLeft: 30 }}
@@ -448,8 +485,9 @@ export default function TimeTableScreen({ navigation }) {
             <Image
               source={{ uri: image3 }}
               style={{
+                borderRadius:3,
                 height: 200,
-                width: 350,
+                width: 300,
               }}
             />
 
@@ -461,6 +499,7 @@ export default function TimeTableScreen({ navigation }) {
                 color="#02b2f2"
               />
             </View>
+          </View>
           </View>
         </ScrollView>
       </View>
@@ -494,7 +533,7 @@ const styles = StyleSheet.create({
     marginBottom: 20,
     borderRadius: 10,
     marginTop: -10,
-    height: 500,
+    height: '100%',
     backgroundColor: "white",
     marginHorizontal: 1,
     shadowColor: "#000",
@@ -506,35 +545,14 @@ const styles = StyleSheet.create({
     shadowRadius: 10,
     elevation: 0.001,
   },
-  homeContent: {
-    alignSelf: "center",
-    alignItems: "center",
-    marginTop: 10,
-    marginBottom: 15,
-    backgroundColor: "#f2ffff",
-    height: 120,
-    width: 290,
-    borderRadius: 10,
-    shadowColor: "#000",
-    shadowOffset: {
-      width: 0,
-      height: 1,
-    },
-    shadowOpacity: 2,
-    shadowRadius: 5,
-    elevation: 8,
-  },
-  homeContentText: {
-    alignSelf: "center",
-    marginTop: 30,
-    fontSize: 30,
-  },
+ 
+ 
   Box: {
     marginBottom: 15,
     marginTop: 15,
     marginLeft: 5,
     marginRight: 5,
-    backgroundColor: "#f5feff",
+    //backgroundColor: "#a3eaff",
     borderRadius: 5,
     marginHorizontal: 1,
     borderRadius: 10,
@@ -545,37 +563,11 @@ const styles = StyleSheet.create({
     },
     shadowOpacity: 0.5,
     shadowRadius: 5,
-    elevation: 8,
+    elevation: 6,
   },
-  Msg: {
-    marginLeft: 30,
-    marginTop: 20,
-    marginRight: 30,
-
-    borderRadius: 2,
-  },
-  pic: {
-    alignSelf: "center",
-    marginTop: 10,
-  },
-  title: {
-    marginTop: 30,
-    alignSelf: "center",
-    fontSize: 25,
-    fontWeight: "bold",
-  },
-  msgText: {
-    marginLeft: 150,
-    marginTop: 5,
-    marginBottom: 5,
-    fontSize: 10,
-  },
-  msg: {
-    fontSize: 15,
-    marginBottom: 20,
-    marginLeft: 10,
-    marginRight: 5,
-  },
+ 
+ 
+ 
   Loadingcontainer: {
     flex: 1,
 
@@ -586,8 +578,17 @@ const styles = StyleSheet.create({
   avatar: {
     marginTop: 20,
     marginBottom: 30,
-
+    borderRadius:3,
     alignSelf: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 5,
+    elevation: 5,
+    
   },
   uploadButton: {
     alignItems: "flex-end",
