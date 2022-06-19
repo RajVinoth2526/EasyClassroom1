991c3a24b6bd0c004de5f51fb8edc0b4018c0b0b
minshaf998 <minshaf998@gmail.com>
2022-06-19 16:11:33 +0530

    payscreen bug

--- app/screens/Payment/PayScreen.js
+++ app/screens/Payment/PayScreen.js
@@ -1,7 +1,5 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
-// import "payhere-embed-sdk/dist/react.css"
-import Payhere from "payhere-embed-sdk/dist/react"
 
 export default function PayScreen() {
     const [success, setSuccess] = useState(false)
